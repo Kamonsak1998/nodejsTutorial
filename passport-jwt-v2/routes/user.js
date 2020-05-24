0a7979f9f700779dc5a931eb613a7be4c421a057
Kamonsak1998 <Kamonsak_1998@hotmail.com>
2020-05-24 18:44:45 +0700

    update validate

--- passport-jwt-v2/routes/user.js
+++ passport-jwt-v2/routes/user.js
@@ -10,6 +10,7 @@ router.get('/profile',userController.getProfile)
 // update user
 router.put('/profile',[
     check('email','The system does not allow email updates.').isEmpty(),
+    check(['firstName','lastName','birthDate'],'first name or last name or birthDate should not be empty').not().isEmpty(),
     check('password','Password should not be empty, minimum eight characters, at least one letter, one number and one special character').isLength({min: 8})
 ],userController.updateProfile)
 
